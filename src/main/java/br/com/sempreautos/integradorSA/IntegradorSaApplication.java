d06e679633aa60cd275c1fd4230074a7afa1ef31
fabiofsrj <fabio.silva@fabiosilva.wdev.int>
2017-09-29 10:05:04 -0300

    Alteração para transformar app em war deployable

--- src/main/java/br/com/sempreautos/integradorSA/IntegradorSaApplication.java
+++ src/main/java/br/com/sempreautos/integradorSA/IntegradorSaApplication.java
@@ -2,10 +2,19 @@ package br.com.sempreautos.integradorSA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import com.sun.glass.ui.Application;
 
 @SpringBootApplication
-public class IntegradorSaApplication {
+public class IntegradorSaApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(IntegradorSaApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(IntegradorSaApplication.class, args);
 	}
